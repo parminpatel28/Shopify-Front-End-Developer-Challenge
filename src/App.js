313e7c2b2eb06f47e8c3c897fe0dfd32aab2f176
parminpatel28 <ps26pate@uwaterloo.ca>
2021-04-29 09:29:57 -0400

    fixing vercel deploy

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import SearchBar from "./components/Search";
 import SearchResults from "./components/SearchResults";
+import SearchBar from "./components/Search";
 import AddNomination from "./components/AddNomination";
 import RemoveNomination from "./components/RemoveNomination";
 import Notification from "./components/Notification";
