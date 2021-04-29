8196a8e22eef33a5c14666e5fda6591f243f8ee3
parminpatel28 <ps26pate@uwaterloo.ca>
2021-04-29 09:36:40 -0400

    fixing file naming

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import SearchResults from "./components/SearchResults";
+import SearchResults from "./components/SearchResult";
 import SearchBar from "./components/Search";
 import AddNomination from "./components/AddNomination";
 import RemoveNomination from "./components/RemoveNomination";
